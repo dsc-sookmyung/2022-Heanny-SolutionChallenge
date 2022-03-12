f8beeff73b4ca6b99172a5afbc8d8153bad5bfcd
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-03-13 04:27:27 +0900

    [feat] 글작성 spinner 1차 구현

--- NoLFI/app/src/main/java/com/example/nolfi/write/SellActivity.java
+++ NoLFI/app/src/main/java/com/example/nolfi/write/SellActivity.java
@@ -3,6 +3,11 @@ package com.example.nolfi.write;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.nolfi.R;
 
