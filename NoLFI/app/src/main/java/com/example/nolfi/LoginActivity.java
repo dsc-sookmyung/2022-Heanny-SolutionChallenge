d7aeab0d1daab189d2b3e68b983703a576dc5208
Kyungmin Lee <min2000.lee@gmail.com>
2022-03-13 23:19:18 +0900

    로그인 회원가입 기능 구현
    register 화면 완성 & 이메일+비번 firebase 실시간 저장 기능 구현

---
+++ NoLFI/app/src/main/java/com/example/nolfi/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.nolfi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        Button btn_register=findViewById(R.id.loginSignUpBtn);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 회원가입 화면으로 이동
+                Intent intent=new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ NoLFI/app/src/main/java/com/example/nolfi/RegisterActivity.java
@@ -0,0 +1,128 @@
+package com.example.nolfi;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;         // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef;     // 실시간 데이터베이스
+
+    private EditText mEtEmail, mEtPwd;          // 회원가입 입력필드
+    private Button mBtnRegister, mBtnImage;     // 회원가입 버튼
+
+    ImageView imageView;
+    Uri selectedImageURi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("NolFI");
+
+        mEtEmail=findViewById(R.id.registerPhoneNum);
+        mEtPwd=findViewById(R.id.registerPassword);
+        mBtnRegister=findViewById(R.id.registerSignUpBtn);
+
+        //mBtnImage=findViewById(R.id.register_image_btn);
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strEmail=mEtEmail.getText().toString();
+                String strPwd=mEtPwd.getText().toString();
+
+                // Firebase Auth 진행
+                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser=mFirebaseAuth.getCurrentUser();
+                            UserAccount account=new UserAccount();
+                            account.setIdToken(firebaseUser.getUid());  // 로그인하면 부여되는 고유값
+                            account.setEmailId(firebaseUser.getEmail());
+                            account.setPassword(strPwd);
+
+                            // setValue: database에 삽입하는 행위
+                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                            Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "회원가입에 실패하셨습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        //image 클릭시
+        imageView=(ImageView) findViewById(R.id.register_image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //갤러리에서 이미지 클릭해서 이미지 뷰에 보여주기
+                Intent intent=new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/");
+                startActivityForResult(intent,1);
+            }
+        });
+
+        // https://stickode.tistory.com/7
+
+        /*
+        mBtnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 0);
+            }
+        });
+        */
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null & data.getData() != null) {
+            selectedImageURi = data.getData();
+            imageView.setImageURI(selectedImageURi);
+        }
+    }
+
+
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==0) {
+            if (resultCode==RESULT_OK) {
+                // Glide.with(getApplicationContext()).load(data.getData()).override(500, 500).into(imageView);
+                // Glide.with(getApplicationContext())
+            }
+        }
+    }
+    */
+}
