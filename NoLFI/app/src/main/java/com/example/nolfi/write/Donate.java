c28c5038d10a69f281d2b89613b44d0e0dae778e
Jinyoung Kim <khappy9888@sookmyung.ac.kr>
2022-03-16 00:42:09 +0900

    [feat] dropdown menu 기능 구현 완료

--- NoLFI/app/src/main/java/com/example/nolfi/write/Donate.java
+++ NoLFI/app/src/main/java/com/example/nolfi/write/Donate.java
@@ -7,9 +7,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -25,18 +29,55 @@ public class Donate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
-
+        ImageView imageView3=(ImageView) findViewById(R.id.down_arrow3);
         //image 클릭시
-        img_donate=(ImageView) findViewById(R.id.donate_product_photo);
+        img_donate = (ImageView) findViewById(R.id.donate_product_photo);
         img_donate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //갤러리에서 이미지 클릭해서 이미지 뷰에 보여주기
-                Intent intent=new Intent(Intent.ACTION_PICK);
-                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/");
-                startActivityForResult(intent,1);
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/");
+                startActivityForResult(intent, 1);
             }
         });
+
+        registerForContextMenu(imageView3);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu,
+                                    View v,
+                                    ContextMenu.ContextMenuInfo menuInfo)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.write_menu, menu);
+    }
+
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            //sell 클릭
+            case R.id.write_menu1:
+                Intent it0 = new Intent(Donate.this, SellActivity.class);
+                it0.putExtra("page_value", "Sell");
+                Toast.makeText(getApplicationContext(), "Sell", Toast.LENGTH_LONG).show();
+                startActivity(it0);
+                finish();
+                return true;
+
+                //group purchase 클릭
+            case R.id.write_menu2:
+                Intent it1 = new Intent(Donate.this, GroupPurchase.class);
+                it1.putExtra("page_value", "Group Purchase");
+                Toast.makeText(getApplicationContext(), "Group buying click", Toast.LENGTH_LONG).show();
+                startActivity(it1);
+                finish();
+                return true;
+
+        }
+
+        return super.onContextItemSelected(item);
     }
 
     @Override
@@ -47,5 +88,4 @@ public class Donate extends AppCompatActivity {
             img_donate.setImageURI(selectedImageURi);
         }
     }
-
 }
--- NoLFI/app/src/main/java/com/example/nolfi/write/GroupPurchase.java
+++ NoLFI/app/src/main/java/com/example/nolfi/write/GroupPurchase.java
@@ -2,7 +2,18 @@ package com.example.nolfi.write;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.nolfi.R;
 
@@ -12,5 +23,41 @@ public class GroupPurchase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_purchase);
+        ImageView imageView2=(ImageView) findViewById(R.id.down_arrow2);
+        registerForContextMenu(imageView2);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu,
+                                    View v,
+                                    ContextMenu.ContextMenuInfo menuInfo)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.write_menu, menu);
+    }
+
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            //sell 클릭
+            case R.id.write_menu1:
+                Intent it0 = new Intent(GroupPurchase.this, SellActivity.class);
+                it0.putExtra("page_value", "Sell");
+                Toast.makeText(getApplicationContext(), "Sell", Toast.LENGTH_LONG).show();
+                startActivity(it0);
+                finish();
+                return true;
+
+                //donate 클릭
+            case R.id.write_menu3:
+                Intent it2 = new Intent(GroupPurchase.this, Donate.class);
+                it2.putExtra("page_value", "Donate");
+                startActivity(it2);
+                finish();
+                return true;
+
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
