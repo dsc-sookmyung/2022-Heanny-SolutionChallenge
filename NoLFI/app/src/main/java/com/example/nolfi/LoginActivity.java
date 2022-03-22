4d40ebcc8c1aaa5888ecbb14b2a352f339b86b47
Kyungmin Lee <min2000.lee@gmail.com>
2022-03-23 01:31:20 +0900

    store와 customer 회원 분리
    userStatus=0이면 store, userStatus=1이면 customer 회원

--- NoLFI/app/src/main/java/com/example/nolfi/LoginActivity.java
+++ NoLFI/app/src/main/java/com/example/nolfi/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.nolfi;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -30,10 +32,9 @@ public class LoginActivity extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 메인 화면으로 이동
-                Intent intent=new Intent(LoginActivity.this, MainActivity.class);
+                // 메인 화면으로  이동
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
-
             }
         });
     }
