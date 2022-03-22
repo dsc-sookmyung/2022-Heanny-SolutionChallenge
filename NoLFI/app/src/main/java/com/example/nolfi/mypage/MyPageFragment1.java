29a59c3f7a504b5ef08ea78117f78221eea7cd76
Jueunkim429 <jueunkim429@gmail.com>
2022-03-22 21:35:07 +0900

    mypage_main finish

--- NoLFI/app/src/main/java/com/example/nolfi/mypage/MyPageFragment1.java
+++ NoLFI/app/src/main/java/com/example/nolfi/mypage/MyPageFragment1.java
@@ -1,8 +1,11 @@
 package com.example.nolfi.mypage;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,7 +16,50 @@ public class MyPageFragment1 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_mypage1,container,false);
-        return v;
 
-    }
+        Button bookButton = (Button)v.findViewById(R.id.btn_bookmark);
+        Button productButton = (Button)v.findViewById(R.id.btn_productlist);
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MyPage_Bookmark.class);
+                startActivity(intent);
+            }
+        });
+
+        productButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Mypage_Product.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        /*Button btn1 = (Button)v.findViewById(R.id.btn_bookmark);
+        Button btn2 = (Button)v.findViewById(R.id.btn_productlist);
+
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);*/
+
+
+        return v;
+        }
+
+        /*public void onClick(View view){
+
+            switch (view.getId()){
+                case R.id.btn_bookmark:
+                    View(frag);
+                case R.id.btn_productlist:
+                    btnNum = 1;
+                    Log.d("onclick","btnNum : "+ btnNum);
+            }
+        }*/
+
+
+
+
 }
